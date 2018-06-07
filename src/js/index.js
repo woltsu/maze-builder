4e0a9417acabf70998f82f183b9d69075dee3959
Olli <oltsuwh@gmail.com>
2018-06-07 23:45:21 +0300

    changed camera coordinates

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
-camera.position.set( 85, 75, 100 )
+camera.position.set( 85, 73, 100 )
 
 let fps, now, then, elapsed
 fps = 60
